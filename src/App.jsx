facbaa4c91d4cca819834a1dcd78c9f7fea83f20
NavodContrath <navod.contrath@gmail.com>
2025-07-03 13:13:24 +0200

    BONUS

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ function debounce(callback, delay) {
 function App() {
   //state dinamici
   const [searchQuery, setSearchQuery] = useState("")
+  const [product, setProduct] = useState([])
   const [products, setProducts] = useState([])
 
   const loadProducts = useCallback(debounce((query) => {
@@ -31,6 +32,16 @@ function App() {
     console.log(products)
   }, [products])
 
+  const getProduct = async (id) => {
+    const res = await fetch(`http://localhost:3333/products/${id}`)
+    const data = await res.json()
+    setProduct(data)
+  }
+
+  useEffect(() => {
+    console.log(product)
+  }, [product])
+
   return (
     <>
       <header className="bar d-flex align-items-center justify-content-center position-relative">
@@ -41,13 +52,28 @@ function App() {
             <ul className='suggestions'>
               {products.map(p => {
                 return (
-                  <li key={p.id}>{p.name}</li>
+                  <li key={p.id} onClick={() => { getProduct(p.id) }}>{p.name}</li>
                 )
               })}
             </ul>
           )
         }
       </header>
+      <main>
+        {
+          product && (
+            <div class="card">
+              <img class="card-img-top" src={`${product.image}`} alt="Title" />
+              <div class="card-body">
+                <h4 class="card-title">{product.name}</h4>
+                <h4 class="card-subtitle text-primary">{product.brand}</h4>
+                <p class="card-text text-danger">{product.price}</p>
+              </div>
+            </div>
+
+          )
+        }
+      </main>
     </>
   )
 }
