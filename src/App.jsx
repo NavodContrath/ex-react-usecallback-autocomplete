c369e01821e40f3e8e5427b8ab0f59feaf888114
NavodContrath <navod.contrath@gmail.com>
2025-07-03 12:24:37 +0200

    MILESTONE-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
-
+import { useMemo, useState, useCallback, useEffect } from 'react'
 function App() {
+  const [searchQuery, setSearchQuery] = useState("")
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    async function loadProducts(query) {
+      const res = await fetch(`http://localhost:3333/products?search=${query}`)
+      const data = await res.json()
+      setProducts(data)
+    }
+    loadProducts(searchQuery)
+  }, [searchQuery])
+  console.log(products)
 
   return (
     <>
-      <header className="bar d-flex align-items-center justify-content-center">
-        <input type="text" name="serach-politician" id="search-bar" /* value={searchQuery} onChange={(e) => { setSearchQuery(e.target.value) }} */ />
+      <header className="bar d-flex align-items-center justify-content-center position-relative">
+        <input type="text" name="serach-politician" id="search-bar" value={searchQuery} onChange={(e) => { setSearchQuery(e.target.value) }} />
         <button type="button">Search</button>
+        {
+          searchQuery && products.length > 0 && (
+            <ul className='suggestions'>
+              {products.map(p => {
+                return (
+                  <li key={p.id}>{p.name}</li>
+                )
+              })}
+            </ul>
+          )
+        }
       </header>
     </>
   )
