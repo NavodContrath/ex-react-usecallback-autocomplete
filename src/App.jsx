a22ac77f39769242f1d396e4201bce9ac3df4f88
NavodContrath <navod.contrath@gmail.com>
2025-07-03 12:03:17 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+  return (
+    <>
+      <header className="bar d-flex align-items-center justify-content-center">
+        <input type="text" name="serach-politician" id="search-bar" /* value={searchQuery} onChange={(e) => { setSearchQuery(e.target.value) }} */ />
+        <button type="button">Search</button>
+      </header>
+    </>
+  )
+}
+
+export default App
